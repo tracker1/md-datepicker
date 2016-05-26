b47dda61a375f9814235fdc953fdc735c8409087
Michael J. Ryan <Michael.J.Ryan2@aexp.com>
2016-05-26 11:32:43 -0700

    tweak to focus container on open.

--- _src/DatePickerModal/feature/initialize/render-component.js
+++ _src/DatePickerModal/feature/initialize/render-component.js
@@ -2,10 +2,12 @@ import { Provider } from 'react-redux';
 import App from '../main';
 
 export default function renderComponent(refs) {
-  return ReactDOM.render(
+  const ret = ReactDOM.render(
     <Provider store={refs.store}>
       <App />
     </Provider>,
     refs.container
   );
+  refs.container.focus();
+  return ret;
 }
